2b1304191e031adc4921b9af87c0c497355ec094
MichnevicDiana <michnevic.diana@gmail.com>
2020-11-04 21:33:09 +0200

    Array new

--- MyExample/src/application/Application.java
+++ MyExample/src/application/Application.java
@@ -1,10 +1,29 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// double random = Math.random();
+		int k = 100;
+
+		if (!"".equals(args[0])) {
+
+			k = Integer.parseInt(args[0]);
+		}
+
+		ArrayList<Double> numbers = new ArrayList();
+
+		for (int i = 0; i < k; i++) {
+			numbers.add(Math.random());
+		}
+
+		Collections.sort(numbers);
+		System.out.println(numbers);
 	}
-
 }
